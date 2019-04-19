2d7f6d82ca3b5eff61b328e177a49f330a7da4e6
Andrew <shuganlive@gmail.com>
2019-04-19 15:31:05 +0300

    added register

--- api/app.js
+++ api/app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const cors = require("cors");
-const services = require('./services');
+const {authenticate, register} = require('./services');
 const bodyParser = require("body-parser");
-const authenticate = services.authenticate;
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 const port = 9000;
 const app = express()
@@ -10,7 +9,9 @@ const app = express()
 
 app.use(cors());
 app.get('/', urlencodedParser, (req, res) => res.send('Hello World!'))
-app.get('/auth', urlencodedParser, (req, res) => res.sendFile(__dirname + "/index.html")) //auth поменять на testAPI
+app.get('/auth', urlencodedParser, (req, res) => res.sendFile(__dirname + "/index.html"))
+app.get('/register', urlencodedParser, (req, res) => res.sendFile(__dirname + "/index1.html")) //auth поменять на testAPI
 app.post('/auth', urlencodedParser, authenticate);
+app.post('/register', urlencodedParser, register);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- api/services.js
+++ api/services.js
@@ -3,28 +3,33 @@ const db = require('./db');
 const take = db.take;
 const insert = db.insert;
 
- function validPassword(password) {
+ function validPassword(login, password) {
      return new Promise((resolve, reject)=> {
-        take("ann")
+        take(login)
         .then(res => {
-            const hash = crypto.pbkdf2Sync(password,  "f1784c62fec0080cb64e2ce7c9f0af7f", 1000, 64, `sha256`).toString(`hex`);
+            let salt = res.rows[0].salt.toString('hex');
+           const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha256`).toString(`hex`);
            if (res.rows[0].password === hash) {
            resolve(true);          
+           } else {
+               resolve(false);
            }
            })
+           .catch(err => console.log(err));
      }) 
 }; 
 
- function setPassword(password) { 
+ function setPassword(login, password) { 
        return new Promise((resolve, reject) => {
         const salt = crypto.randomBytes(16).toString('hex');  
         const hash1  = crypto.pbkdf2Sync(password, salt,  
         1000, 64, `sha256`).toString(`hex`); 
-        insert("ann", hash1, salt)
+        insert(login, hash1, salt)
         .then(res => {
             console.log(1);
             resolve(true);
-        });
+        })
+        .catch(err => console.log(err));
        })
 }; 
 
@@ -34,7 +39,7 @@ function authenticate(req, res) {
     take(login)
     .then(result => {
         if (result.rows.length != 0) {
-            validPassword(passwd)
+            validPassword(login, passwd)
             .then(result1 => {
                 if (result1) {
                 res.send("Success!");
@@ -49,10 +54,30 @@ function authenticate(req, res) {
     .catch(err => res.send(err));
 }
 
+function register(req, res) {
+    const {login, passwd} = req.body;
+    take(login)
+    .then(result => {
+        if (result.rows.length == 0) {
+            setPassword(login, passwd)
+            .then(result1 => {
+                if (result1) {
+                    res.send("Success!");
+                } else {
+                    res.send("Ooop smth went wrong")
+                }
+            })
+        } else {
+            res.send("That login already exists!");
+        }
+    })
+    .catch(err => res.send(err));
+}
 
 
 module.exports = {
-    authenticate
+    authenticate,
+    register
 }
 
 
